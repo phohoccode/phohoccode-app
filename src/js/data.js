4a2f224c57605a7f39b4a1255c99c66c7bb1c51a
Nhan Quốc Việt <qviet092@gmail.com>
2024-03-18 23:26:27 +0700

    update code 18.3.2024

---
+++ src/js/data.js
@@ -0,0 +1,155 @@
+const projects_data = [
+    {
+        id: 0,
+        background: './assets/img/zingmp3/Screenshot (64).png',
+        path_images: [
+            './assets/img/zingmp3/Screenshot (64).png',
+            './assets/img/zingmp3/Screenshot (65).png',
+            './assets/img/zingmp3/Screenshot (66).png',
+            './assets/img/zingmp3/Screenshot (67).png',
+            './assets/img/zingmp3/Screenshot (68).png',
+            './assets/img/zingmp3/Screenshot (69).png',
+            './assets/img/zingmp3/Screenshot (70).png',
+            './assets/img/zingmp3/Screenshot (71).png',
+            './assets/img/zingmp3/Screenshot (72).png',
+            './assets/img/zingmp3/Screenshot (73).png',
+            './assets/img/zingmp3/Screenshot (64).png',
+            './assets/img/zingmp3/Screenshot (75).png',
+        ],
+        name: 'Zing Mp3',
+        language: 'HTML, CSS',
+        func: [
+            'Web tĩnh không có chức năng'
+        ],
+        des: 'Zing MP3, một dự án của Zing, là ứng dụng phát nhạc trực tuyến với thư viện âm nhạc đồ sộ, tính năng cá nhân hóa, giao diện thân thiện, và hỗ trợ đa nền tảng. Người dùng có thể tạo danh sách phát cá nhân, chia sẻ âm nhạc trên mạng xã hội và trải nghiệm âm nhạc mọi lúc, mọi nơi.',
+        link: 'https://phohoccode.github.io/ZingMp3-Clone/',
+        link_source_code: 'https://github.com/phohoccode/ZingMp3-Clone'
+    },
+    {
+        id: 1,
+        background: './assets/img/music/Screenshot (76).png',
+        path_images: [
+            './assets/img/music/Screenshot (76).png',
+            './assets/img/music/Screenshot (77).png',
+            './assets/img/music/Screenshot (78).png',
+            './assets/img/music/Screenshot (79).png',
+        ],
+        language: 'HTML, CSS, JS',
+        func: [
+            'Bật nhạc',
+            'Tắt nhạc',
+            'Tua bài hát',
+            'Phát tiếp bài hát',
+            'Lùi lại bài hát',
+            'Phát ngẫu nhiên',
+            'Phát lặp lại',
+            'Tải bài hát',
+            'Phát lặp lại',
+            'Thay đổi giao diện',
+        ],
+        name: 'Music Player Ui',
+        des: 'Dự án âm nhạc của tôi là một ứng dụng mang đến trải nghiệm nghe nhạc tuyệt vời với hàng loạt tính năng đa dạng và tiện ích. Người dùng có thể dễ dàng bật, tắt nhạc, tua bài hát, và thậm chí là lùi lại bài hát một cách linh hoạt. Chức năng phát tiếp bài hát giúp người dùng không bỏ lỡ bất kỳ giai điệu nào, trong khi chế độ phát ngẫu nhiên mang lại sự đa dạng và bất ngờ trong trải nghiệm nghe nhạc. Để tăng thêm tính tương tác, người dùng có thể tải bài hát yêu thích của họ để nghe offline và thay đổi giao diện theo sở thích cá nhân. Với khả năng lặp lại bài hát yêu thích, dự án của tôi sẽ đảm bảo người dùng có thể thưởng thức những giai điệu ưa thích một cách không giới hạn và thoải mái.',
+        link: 'https://phohoccode.github.io/Music/',
+        link_source_code: 'https://github.com/phohoccode/Music'
+    },
+    {
+        id: 2,
+        background: './assets/img/dragonball/Screenshot (80).png',
+        path_images: [
+            './assets/img/dragonball/Screenshot (80).png',
+            './assets/img/dragonball/Screenshot (81).png',
+            './assets/img/dragonball/Screenshot (82).png',
+            './assets/img/dragonball/Screenshot (83).png',
+        ],
+        language: 'HTML, CSS, JS',
+        func: [
+           'Tìm kiếm thẻ',
+           'Xem thông tin thẻ',
+           'Xem video demo'
+        ],
+        name: 'DragonBall Card',
+        des: 'Dự án xem thông tin thẻ 7 viên ngọc rồng là một ứng dụng độc đáo dành cho những người hâm mộ của loạt truyện tranh này. Với tính năng "Tìm kiếm thẻ", người dùng có thể dễ dàng tìm kiếm thông tin về các thẻ theo tên hoặc tính năng cụ thể một cách nhanh chóng. Khi đã tìm thấy thẻ mong muốn, tính năng "Xem thông tin thẻ" sẽ cung cấp thông tin chi tiết về thẻ đó, bao gồm các thông số kỹ thuật, sức mạnh, và mô tả về nhân vật. Ngoài ra, người dùng cũng có thể tận hưởng trải nghiệm hấp dẫn thông qua tính năng "Xem video demo", để thấy thẻ hoạt động trong thực tế. Với dự án này, người dùng sẽ có cơ hội khám phá thế giới đầy màu sắc của 7 viên ngọc rồng một cách thú vị và tiện lợi.',
+        link: 'https://phohoccode.github.io/DragonBall-Card/',
+        link_source_code: 'https://github.com/phohoccode/DragonBall-Card'
+    },
+    {
+        id: 3,
+        background: './assets/img/note/Screenshot (84).png',
+        path_images: [
+            './assets/img/note/Screenshot (84).png',
+            './assets/img/note/Screenshot (85).png',
+            './assets/img/note/Screenshot (86).png',
+            './assets/img/note/Screenshot (87).png',
+            './assets/img/note/Screenshot (88).png',
+            './assets/img/note/Screenshot (89).png',
+            './assets/img/note/Screenshot (90).png',
+        ],
+        language: 'HTML, CSS, JS',
+        func: [
+            'Thêm ghi chú',
+            'Sửa ghi chú',
+            'Xóa ghi chú',
+            'Tìm kiếm ghi chú',
+            'Lưu trữ ghi chú',
+            'Đánh dấu ghi chú quan trọng',
+            'Lưu ghi chú vào thùng rác sau khi xóa',
+            'Tải xuống ghi chú',
+            'Thay đổi giao diện',
+        ],
+        name: 'Ghi chú',
+        des: `
+        Ứng dụng ghi chú của bạn là một công cụ đa chức năng, giúp người dùng tổ chức và quản lý thông tin một cách hiệu quả. Với khả năng "Thêm ghi chú", người dùng có thể nhanh chóng tạo mới các ghi chú để ghi lại ý tưởng, công việc cần làm hoặc thông tin quan trọng khác. Chức năng "Sửa ghi chú" cho phép người dùng chỉnh sửa nội dung của ghi chú một cách linh hoạt và tiện lợi.
+        
+        Để duy trì sự sắp xếp và quản lý hiệu quả, người dùng có thể dễ dàng xóa các ghi chú không cần thiết hoặc đánh dấu ghi chú quan trọng. Tính năng "Tìm kiếm ghi chú" giúp người dùng dễ dàng tìm kiếm thông tin cụ thể trong số lượng lớn các ghi chú. Đồng thời, việc lưu trữ ghi chú và lưu ghi chú vào thùng rác sau khi xóa đảm bảo rằng thông tin được bảo toàn và có thể khôi phục khi cần thiết.
+        
+        Thêm vào đó, tính năng "Tải xuống ghi chú" cho phép người dùng xuất ghi chú ra các định dạng khác nhau để chia sẻ hoặc lưu trữ ngoại tuyến. Cuối cùng, việc thay đổi giao diện giúp cá nhân hóa trải nghiệm người dùng, mang lại sự thoải mái và tiện ích khi sử dụng ứng dụng ghi chú này.`,
+        link: 'https://phohoccode.github.io/Note-App/',
+        link_source_code: 'https://github.com/phohoccode/Note-App'
+
+    },
+    {
+        id: 4,
+        background: './assets/img/weather/Screenshot (92).png',
+        path_images: [
+            './assets/img/weather/Screenshot (91).png',
+            './assets/img/weather/Screenshot (92).png',
+        ],
+        language: 'HTML, CSS, JS',
+        func: [
+            'Xem dự báo thời tiết',
+        ],
+        name: 'Xem thời tiết',
+        des: `Ứng dụng xem thời tiết cung cấp cho người dùng một cách tiện lợi để tra cứu thông tin về thời tiết hiện tại và dự báo trong tương lai. Với chức năng "Tìm kiếm địa điểm", người dùng có thể nhanh chóng nhập địa điểm mong muốn và nhận được thông tin về thời tiết tại đó.`,
+        link: 'https://phohoccode.github.io/Weather-App/',
+        link_source_code: 'https://github.com/phohoccode/Weather-App'
+
+    },
+    {
+        id: 5,
+        background: './assets/img/todo/Screenshot (93).png',
+        path_images: [
+            './assets/img/todo/Screenshot (93).png',
+            './assets/img/todo/Screenshot (94).png',
+            './assets/img/todo/Screenshot (95).png',
+            './assets/img/todo/Screenshot (96).png',
+        ],
+        language: 'HTML, CSS, JS',
+        func: [
+            'Thêm công việc',
+            'Sửa công việc',
+            'Xóa công việc',
+            'Đánh dấu công việc',
+            'Tải xuống nội dung công việc',
+            'Thay đổi giao diện'
+        ],
+        name: 'Todoist',
+        des: 'Ứng dụng Todoist là một công cụ quản lý công việc linh hoạt và tiện lợi. Người dùng có thể dễ dàng thêm, sửa và xóa các công việc trong danh sách của mình. Tính năng "Thêm công việc" cho phép người dùng nhanh chóng tạo mới các nhiệm vụ, trong khi "Sửa công việc" giúp họ điều chỉnh thông tin của các công việc đã có. Việc đánh dấu công việc quan trọng và tải xuống nội dung công việc giúp người dùng tổ chức và chia sẻ thông tin một cách hiệu quả. Cuối cùng, tính năng "Thay đổi giao diện" cho phép người dùng tùy chỉnh giao diện ứng dụng theo sở thích cá nhân, tạo ra trải nghiệm sử dụng độc đáo và thoải mái.',
+        link: 'https://phohoccode.github.io/TodoList-App/',
+        link_source_code: 'https://github.com/phohoccode/TodoList-App'
+
+    },
+    
+]
+
+
+export default projects_data
--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,110 @@
-const menuButton = document.querySelector(".main-container nav .menu-button");
-const closeButton = document.querySelector(".mobile-menu-items .close-button");
-const mainContainer = document.querySelector(".main-container");
-const menuItems = document.querySelector('.menu-items')
+import projects_data from "./data.js"
 
-menuButton.addEventListener("click", () => {
-  mainContainer.classList.add("active");
-});
+const root = (() => {
+    const $ = document.querySelector.bind(document)
+    const projects = $('.projects')
+    const main = $('.main-container')
+    const menuButton = $('.menu-button')
+    const closeButton = $('.close-button')
+    const menuItem = $('.menu-items')
+    const menuItemMobile = $('.mobile-menu-items .menu-items')
+    window.menuItemMobile = menuItemMobile
+    
+    return {
+        handleEvent() {
+            projects.addEventListener('click', (e) => {
+                e.preventDefault()
+                const seeButton = e.target.closest('.see-now')
+                if (seeButton) {
+                    const currentId = Number(seeButton.dataset.index)
+                    const result = projects_data.filter(project => project.id === currentId)
+                    this.renderProjectsDetail(result)
+                }
+            })
 
-closeButton.addEventListener("click", () => {
-  mainContainer.classList.remove("active");
-});
+            menuButton.addEventListener('click', () => {
+                main.classList.add('active')
+            })
 
-menuItems.addEventListener('click', (e) => {
-    const item = e.target.closest('.menu-items li')
-    console.log(item)
-    document.querySelector('li.active').classList.remove('active')
-    item.classList.add('active')
-})
+            closeButton.addEventListener('click', () => {
+                main.classList.remove('active')
+            })
+            
+            menuItem.addEventListener('click', (e) => {
+                const item = e.target.closest('.menu-items li')
+                if (item) {
+                    main.classList.remove('active')
+                    $('.menu-items li.active').classList.remove('active')
+                    item.classList.add('active')
+                }
+            })
+
+            menuItemMobile.addEventListener('click', (e) => {
+                const itemMobile = e.target.closest('.mobile-menu-items .menu-items li')
+                if (itemMobile) {
+                    main.classList.remove('active')
+                }
+            })
+        },
+        renderProjectsDetail(data) {
+            const htmls = data.map(childData => `
+                <video autoplay loop muted>
+                    <source src="./assets/video/bg.mp4" type="video/mp4" />
+                </video>
+                <div class="bg-overlay"></div>
+                <nav>
+                    <div class="logo">
+                        <a href="./index.html">PHOHOCCODE</a>
+                    </div>
+                    <a href="./index.html" class="back-main-page">
+                        <i class="fa-light fa-xmark"></i>
+                    </a>
+                </nav>
+                <div class="project-detail">
+                    <h3 class="title">Hình ảnh dự án</h3>
+                    <div class="images">
+                        ${childData.path_images.map((image) => `
+                            <div class="image">
+                                <img src="${image}" alt="">
+                            </div>
+                        `).join('')}
+                    </div>
+                    <div class="project-info">
+                        <h3 class="project-name">Tên dự án: ${childData.name}</h3>
+                        <span class="project-language">Ngôn ngữ sử dụng: ${childData.language}</span>
+                        <ul class="project-func">
+                            Chức năng:
+                            ${childData.func.map(childFunc => `
+                                <li>${childFunc}</li>
+                            `).join('')}
+                        </ul>
+                        <p class="project-des">Mô tả dự án: ${childData.des}</p>
+                        <div class"project-link">Link trang web: <a target="blank" href="${childData.link}">${childData.link}</a></div>
+                        <a target="blank" href="${childData.link_source_code}" class="view-soucre">Xem mã nguồn dự án</a>
+                    </div>
+                </div>
+            `).join('')
+            main.innerHTML = htmls
+        },
+        renderProjects() {
+            const htmls = projects_data.map((childData, index) => `
+                <div class="project">
+                    <div class="project-background">
+                        <figure>
+                            <img src="${childData.background}" alt="">
+                        </figure>
+                        <a data-index="${index}" href="./project-detail.html" class="see-now">Xem dự án</a>
+                    </div>
+                </div>
+            `).join('')
+            projects.innerHTML = htmls
+        },
+
+        start() {
+            this.renderProjects()
+            this.handleEvent()
+        }
+    }
+})()
+
+root.start()
